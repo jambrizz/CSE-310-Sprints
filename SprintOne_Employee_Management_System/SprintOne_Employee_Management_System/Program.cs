b97f3e2f13b5132bc06cde994a2b91dfc0c7176b
jambrizz <ambrizjovani@gmail.com>
2023-09-25 22:06:16 -0700

    sprint one initial creation on visual studio

---
+++ SprintOne_Employee_Management_System/SprintOne_Employee_Management_System/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SprintOne_Employee_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
