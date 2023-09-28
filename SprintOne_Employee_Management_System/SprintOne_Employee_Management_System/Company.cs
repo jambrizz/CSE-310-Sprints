3e3b1b39c2cd1d3007ce16628678c6162a7d1d7e
jambrizz <ambrizjovani@gmail.com>
2023-09-28 00:34:29 -0700

    menu feature troubleshooting.

--- SprintOne_Employee_Management_System/SprintOne_Employee_Management_System/Company.cs
+++ SprintOne_Employee_Management_System/SprintOne_Employee_Management_System/Company.cs
@@ -10,12 +10,71 @@ public class Company
 	private List<string> employees = new List<string>();
 	public void runCompanyApp()
 	{
-        //string path = _menu;
-		string path = "\\MenuFeature.txt";
-		string[] lines = File.ReadAllLines(path);
-		foreach (string line in lines)
+        string[] menu = File.ReadAllLines("MenuFeature.txt");
+        foreach (string line in menu)
+        {
+            Console.WriteLine(line);
+        }
+        Console.WriteLine("Please enter your choice: ");
+        string choice = Console.ReadLine();
+
+        //TODO: Fix this section of code it causes and error in the build
+        //Maybe change to a do while loop
+
+        /*
+		bool runProgram = true;
+		while (runProgram == true)
 		{
-			Console.WriteLine(line);
-		}
+            int featureChoice = 0;
+            bool validChoice = false;
+			while (validChoice == false)
+			{
+                string[] menu = File.ReadAllLines("MenuFeature.txt");
+                foreach (string line in menu)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine("Please enter your choice: ");
+                string choice = Console.ReadLine();
+
+                validChoice = int.TryParse(choice, out int featureChoice);
+
+                if (validChoice == false)
+                {
+                    Console.WriteLine("Please enter a valid choice.");
+                }
+                else if (featureChoice < 1 || featureChoice > 5)
+                {
+                    Console.WriteLine("Please enter a valid choice.");
+                }
+                else
+                {
+                    validChoice = true;
+                }
+            }
+
+            switch (featureChoice)
+            {
+                case 1:
+                    Console.WriteLine("Add a new employee");
+                    break;
+                case 2:
+                    Console.WriteLine("Update an employee");
+                    break;
+                case 3:
+                    Console.WriteLine("Delete an employee");
+                    break;
+                case 4:
+                    Console.WrtieLine("View all employees");
+                    break;
+                case 5:
+                    Console.WriteLine("Exit");
+                    Console.ReadKey();
+                    runProgram = false;
+                    break;
+            }
+           
+        }
+        */
     }
 }
